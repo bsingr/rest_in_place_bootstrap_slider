06a6f8379ff8027d622e54de350d9c011b6e20d1
Jens Bissinger <mail@jens-bissinger.de>
2014-01-24 07:48:25 +0100

    Bootstrap test app.

---
+++ TestApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '3f9849cb01c50b76b69bb3eac2aa0f7d38997c47e5d5cf7f04d211c5c81dcf6a003ccd3c65b73a2aa93719d9a782befcf00e508e406026f6475048d694646e7f'
